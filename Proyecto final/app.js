437fd2fb61fb1594fb1a40c6b10687eb3f07becb
Augusto Jose Godoy <126405800+Augusto-godoy@users.noreply.github.com>
2023-02-28 15:36:20 -0300

    Add files via upload

---
+++ Proyecto final/app.js
@@ -0,0 +1,22 @@
+//Menu lateral
+var menu_visible = false
+let menu = document.getElementById("nav");
+function mostrarOcultarMenu(){
+    if (menu_visible==false){ //si esta oculto
+         menu.style.display = "block";
+         menu_visible = true;    
+    }
+    else{
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
+// ocultar una vez que elejimos una de las opciones
+let links = document.querySelectorAll("nav a")
+for (var x =0; x<links.length;x++){
+    links[x].onclick= function(){
+        menu.style.display = "none"
+        menu_visible = false;
+    }
+}
+
